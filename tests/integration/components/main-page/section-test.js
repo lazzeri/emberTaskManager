d7fb44af111d3950b53a7496daec7b5b5b50cb4e
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2024-05-09 21:57:35 +0200

    create static Section Item

---
+++ tests/integration/components/main-page/section-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'task-manager/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | main-page/section', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<MainPage::Section />`);
+
+    assert.dom().hasText('');
+
+    // Template block usage:
+    await render(hbs`
+      <MainPage::Section>
+        template block text
+      </MainPage::Section>
+    `);
+
+    assert.dom().hasText('template block text');
+  });
+});
