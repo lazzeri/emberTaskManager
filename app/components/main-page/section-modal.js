a3902f2222d9d4d4e6f63017d36bd360fc30d2d5
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2024-05-12 09:45:36 +0200

    create bass css structure for modal

--- app/components/main-page/section-modal.js
+++ app/components/main-page/section-modal.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
 
-export default class MainPageSectionModalComponent extends Component {
+export default class MainPageSectionModalComponent extends Component
+{
+  modal = this.args.modal;
+
 
   //If i would want to use the item, I could do like this.
   //item = this.args.item;
